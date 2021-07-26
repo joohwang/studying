bb4c7bc5626cfa2d47ef1f4b3a457a2225a24aff
skysylph <skysylph123@gmail.com>
2021-07-26 23:47:22 +0900

    1

--- index.ts
+++ index.ts
@@ -1,43 +1,66 @@
 import { Bundler, defaultPlugins } from "https://deno.land/x/bundler/mod.ts";
 import * as path from "https://deno.land/std@0.102.0/path/mod.ts";
 
-const defaultBundle = async (inputPath: string | undefined, outputPath: string | undefined) => {
+const defaultBundle = async (
+  inputPath: string | undefined,
+  outputPath: string | undefined
+) => {
+  const plugins = defaultPlugins();
+  const bundler = new Bundler(plugins);
+  const input = inputPath || "src/index.html";
+  const output = outputPath || "index.html";
+  const outputMap = { [input]: output };
 
-    const plugins = defaultPlugins()
-    const bundler = new Bundler(plugins);
-    const input = inputPath || "src/index.html";
-    const output = outputPath || "index.html";
-    const outputMap = { [input]: output };
+  console.log("outputMap >>>>>>>>>>>> ", inputPath, output);
+
+  const graph = await bundler.createGraph([input], { outputMap });
+  const chunks = await bundler.createChunks([input], graph);
+  const bundle: object = await bundler.createBundles(chunks, graph);
+
+  for (let prop in bundle) {
+    let val : string = bundle[prop];
+
+    console.log(val, typeof val);
+
+    const encdoer = new TextEncoder();
+    const data: Uint8Array = encdoer.encode(val);
+
+    /*
+        if (typeof val == "string") {
+            
+            const encdoer = new TextEncoder();
+            const data: Uint8Array = encdoer.encode(val);
 
 
-    console.log("outputMap >>>>>>>>>>>> ", inputPath, output);
+            let parser = path.parse(prop);
+            let newPath = path.join('deps', parser.name.concat('js'));
 
-    const graph = await bundler.createGraph([input], { outputMap });
-    const chunks = await bundler.createChunks([input], graph);
-    const bundle = await bundler.createBundles(chunks, graph);
+            const newFile = await Deno.create(newPath);
 
-    for (let prop in bundle) {
 
-    }
+            Deno.write(newFile.rid, data);
+            Deno.close(newfile.rid);
 
-    console.log("bundles >>>>>>>>>>>> ", bundle);
 
-}
+
+            console.log(path.parse(prop));
+
+        }
+        */
+  }
+
+  console.log("bundles >>>>>>>>>>>> ", bundle);
+};
 
 const cwd = Deno.cwd();
 
 const watcher = await Deno.watchFs(`${cwd}/src`, { recursive: true });
 for await (const evt of watcher) {
-
-    let paths = evt.paths;
-    for await (let p of paths) {
-
-        console.log("fromFileUrl  ", path.fromFileUrl(path.toFileUrl(p).href));
-        p = p.replace(cwd.concat('/'), '');
-        defaultBundle(p, p);
-    }
-
+  let paths = evt.paths;
+  for await (let p of paths) {
+    p = p.replace(cwd.concat("/"), "");
+    defaultBundle(p, p);
+  }
 }
-
 
 defaultBundle(undefined, undefined);
--- src/text_editor.ts
+++ src/text_editor.ts
@@ -2,6 +2,15 @@ import { LitElement, html, css } from 'https://cdn.skypack.dev/lit';
 import { customElement, property } from 'https://cdn.skypack.dev/lit/decorators';
 
 @customElement("text-editor")
-export class TextEditor {
+export class TextEditor extends LitElement {
+    @property()
+    name = "hello";
+
+    @property()
+    value = 1;
+    
+    render() {
+        return html`<div>${value}  : <span>${name}</span> </div>`;
+    }
 
 }
